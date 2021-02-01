0c7a31fb2a4600c4b6228b89c2ffbf67f8b2df8e
tryggve.sorensen <tryggve.sorensen@pgs.com>
2021-02-01 16:19:29 +0100

    Uninon args and comments, documentation example.

--- Union_args.py
+++ Union_args.py
@@ -7,6 +7,17 @@ import datetime
 from typing import Union, List, Dict, Tuple
 
 
+############################################################################
+# NOTE: The type "declarations" in the function specification is not being
+# evaluated. This is so-called annotations and is described in the Python
+# reference documentation.
+# https://docs.python.org/3/tutorial/controlflow.html#function-annotations
+# https://www.python.org/dev/peps/pep-0484/
+# (PEP - Python Enhanced Proposal)
+#
+# Apparently there are validator applications that can check python code, maybe
+# something to look into...
+############################################################################
 def my_function(param1: Union[str, int]) -> None:
     # Check the type of the parameter
     if isinstance(param1, int):
@@ -21,20 +32,23 @@ def my_function(param1: Union[str, int]) -> None:
     print(param1)
 
 
-def dp_insert(datapoints: Union[List[Dict[Union[int, float, datetime.datetime],
-                                          Union[int, float, str]]],
-                                List[Tuple[Union[int, float, datetime.datetime],
-                                           Union[int, float, str]]]],
-              id: int = None, external_id: str = None) -> None:
+def cog_datapointsapi_insert(datapoints: Union[List[Dict[Union[int, float, datetime.datetime], Union[int, float, str]]],
+                                               List[Tuple[Union[int, float, datetime.datetime], Union[int, float, str]]]],
+                             id: int = None, external_id: str = None) -> None:
     pass
 
 
-
 def main():
-
+    # This call is triggering the error message in my_function
     my_function([10, 10])
+
+    # This call is the str parameter
     my_function("Hello world!")
+
+    # This call is the int parameter
+    my_function(10)
 
 
 if __name__ == '__main__':
+    print('The value of __name__ is ' + __name__)
     main()

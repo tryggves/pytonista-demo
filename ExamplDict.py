721b1409e3a03f04f8786e1a0268ee40c2369e3b
tryggves <tryggve.sorensen@gmail.com>
2021-02-03 09:49:23 +0100

    Fixing indents.

--- ExamplDict.py
+++ ExamplDict.py
@@ -5,10 +5,10 @@
 print()
 print('===============')
 print('Demo dictionaries...')
-person={'name' : 'Per Parker',
-				'address' : 'Nordpolveien 68',
-				'telefon' : '83244'}
-				
+person = {'name': 'Per Parker',
+          'address': 'Nordpolveien 68',
+          'telefon': '83244'}
+
 print(person)
 # Access mapping the same way as using index in list
 print('name is:', end=' ')
@@ -16,21 +16,21 @@ print(person['name'])
 
 # Mapping elements can be changed
 print('Assign new name to person')
-person['name']='Pilen Palle'
+person['name'] = 'Pilen Palle'
 print(person)
 print('name is:', end=' ')
 print(person['name'])
 
 # Now extend to nested structures
 print('Nested structure')
-person['name']={'first' : 'Pilen', 'last' : 'Palle'}
-person['telefon']=['83244','84234']
+person['name'] = {'first': 'Pilen', 'last': 'Palle'}
+person['telefon'] = ['83244', '84234']
 print(person)
 print('name is:', end=' ')
 
 # Here we index into the nested structure, cool
 # Special separator between first and last name
-print(person['name']['first'],person['name']['last'],sep='////')
+print(person['name']['first'], person['name']['last'], sep='////')
 print('telefon: ', person['telefon'][0])
 
 person['telefon'].append('66808420')

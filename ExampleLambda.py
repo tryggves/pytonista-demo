611301c595c742dd8df1e09bf21e5092342e91cf
tryggves <tryggve.sorensen@gmail.com>
2021-02-12 08:47:43 +0100

    Lambda example.
    Some refactoring an more examples added.

--- ExampleLambda.py
+++ ExampleLambda.py
@@ -4,6 +4,7 @@
 #
 # https://realpython.com/python-lambda/
 # https://realpython.com/python-map-function/
+# https://realpython.com/python-reduce-function/
 #
 # Lambda expression contains:
 # keyword "lambda"
@@ -36,10 +37,13 @@ print("Example with function.")
 print(f'3 square is {my_sqr(3)}')
 print(f'4 square is {my_sqr(4)}')
 
-# Try this out with lambda expression
+# Try this out with lambda expression - this is not a best practice.
+# The lambda expression here makes it harder to read.
 print('Example with lambda')
 print(f'3 square is {(lambda x: x**2)(3)}')
 print(f'4 square is {(lambda x: x**2)(4)}')
+for i in range(10):
+    print(f'sqr({i})={my_sqr(i)}')
 
 # Passing varargs
 print(f'varargs {(lambda *args: sum(args))(1,2,3)}')
@@ -47,10 +51,11 @@ print(f'varargs {(lambda *args: sum(args))(1,2,3)}')
 print(f'kwargs {(lambda **kwargs: sum(kwargs.values()))(en=1, to=2, tre=3)}')
 
 # Unsure about the second argument
-print((lambda x, *, y=0, z=0: x + y + z)(1, y=2, z=3))
-print((lambda x, y=0, z=0: x + y + z)(1, y=2, z=3))
+print(f'Argument ex 1: {(lambda x, *, y=0, z=0: x + y + z)(1, y=2, z=3)}')
+print(f'Argument ex 2: {(lambda x, y=0, z=0: x + y + z)(1, y=2, z=3)}')
 # This does not work
 # print((lambda x, *, y=0, z=0: x + y + z)(1, 2, 3, y=4, z=5))
+print(f'Argument ex 3: {(lambda u, v, y=0, z=0: u + v + y + z)(1, 2, y=4, z=5)}')
 
 
 ################################################################################
@@ -88,77 +93,3 @@ print("CLOSURE WITH INNER LAMBDA EXPRESSION")
 for ii in range(3):
     closure = outer_func_l(ii)
     print(f"closure({ii + 5}) = {closure(ii + 5)}")
-################################################################################
-################################################################################
-# Map example 1. Create a list and perform a function on each element of the list
-print("Map example 1")
-in_list = [1, 2, 3, 4, 5]
-# map() returns an iterator of the result of applying the function on the in_list
-res_iter = map(lambda x: x * 2, in_list)
-res_list = list(res_iter)
-print(res_list)
-
-
-################################################################################
-# Map example 2. Create a custom function to be applied in the map
-def my_func(x):
-    return x + 10
-
-
-print("Map example 2")
-# This is the typical pattern where the result list is created from the map iterator
-res_list = list(map(my_func, in_list))
-print(res_list)
-
-################################################################################
-# Example 3: Crete numbers from strings
-print("Map example 3")
-res_list = list(map(float, ["12.2", "4.456", "5"]))
-print(res_list)
-
-
-################################################################################
-# Example 4: Numbers from strings with possible errors
-# Define custom function
-def to_float(x: str):
-    try:
-        return float(x.replace(',', '.'))
-    except ValueError:
-        return float("nan")
-
-
-print("Map example 4")
-my_numbers = ["10,8", "58", "realli not a number"]
-
-res_list = list(map(to_float, my_numbers))
-print(res_list)
-
-
-################################################################################
-# Example 5: Map and filter function
-# filter() applies a predicate (true,false) function on the iterable
-def is_positive(num):
-    return num >= 0
-
-
-def sanitized_sqrt(numbers):
-    # cleaned_iter = map(math.sqrt, filter(is_positive, numbers))
-    # Use lambda expression in filter()
-    cleaned_iter = map(math.sqrt, filter(lambda num: num >= 0, numbers))
-    return list(cleaned_iter)
-
-
-print("Map and filter example 5")
-res_list = sanitized_sqrt([25, 9, 81, -16, 0])
-print(res_list)
-
-
-#####################################################################################
-# Example 6: Map and reduce() function
-# Reduce aggregates values from the list/iterator returned by map
-print("Map and reduce example 6")
-home_dir = os.path.expanduser("~")
-files_dirs = os.listdir(home_dir)
-files = list(map(lambda file_name: home_dir + "/" + file_name, files_dirs))
-aggregate_size = functools.reduce(operator.add, map(os.path.getsize, files))
-print(f'Aggregate size is {aggregate_size}')
---
+++ ExampleMap.py
@@ -0,0 +1,129 @@
+#
+# Examples of map(), filter(), reduce()
+# List comprehension
+#
+
+import math
+import functools
+import operator
+import os.path
+
+################################################################################
+################################################################################
+# Map example 1. Create a list and perform a function on each element of the list
+print("Map example 1")
+in_list = [1, 2, 3, 4, 5]
+# map() returns an iterator of the result of applying the function on the in_list
+res_iter = map(lambda x: x * 2, in_list)
+res_list = list(res_iter)
+print(res_list)
+
+
+################################################################################
+# Map example 2. Create a custom function to be applied in the map
+def my_func(x):
+    return x + 10
+
+
+print("Map example 2")
+# This is the typical pattern where the result list is created from the map iterator
+res_list = list(map(my_func, in_list))
+print(res_list)
+
+################################################################################
+# Example 3: Crete numbers from strings
+print("Map example 3")
+res_list = list(map(float, ["12.2", "4.456", "5"]))
+print(res_list)
+
+
+################################################################################
+# Example 4: Numbers from strings with possible errors
+# Define custom function
+def to_float(x: str):
+    try:
+        return float(x.replace(',', '.'))
+    except ValueError:
+        return float("nan")
+
+
+print("Map example 4")
+my_numbers = ["10,8", "58", "realli not a number"]
+
+res_list = list(map(to_float, my_numbers))
+print(res_list)
+
+
+################################################################################
+# Example 5: Map and filter function
+# filter() applies a predicate (true,false) function on the iterable
+def is_positive(num):
+    return num >= 0
+
+
+def sanitized_sqrt(numbers):
+    # cleaned_iter = map(math.sqrt, filter(is_positive, numbers))
+    # Use lambda expression in filter()
+    cleaned_iter = map(math.sqrt, filter(lambda num: num >= 0, numbers))
+    return list(cleaned_iter)
+
+
+print("Map and filter example 5")
+res_list = sanitized_sqrt([25, 9, 81, -16, 0])
+print(res_list)
+
+
+#####################################################################################
+# Example 6: Map and reduce() function
+# Reduce aggregates values from the list/iterator returned by map
+print("Map and reduce example 6")
+home_dir = os.path.expanduser("~")
+files_dirs = os.listdir(home_dir)
+files = list(map(lambda file_name: home_dir + "/" + file_name, files_dirs))
+aggregate_size = functools.reduce(operator.add, map(os.path.getsize, files))
+print(f'Aggregate size is {aggregate_size}')
+
+
+#####################################################################################
+# Example 7: List comprehension
+
+# Here we generate a list using map funcion
+print("Map list")
+res_list = list(map(to_float, my_numbers))
+print(res_list)
+
+print("List comprehension list")
+# Here is the same using list comprehension
+lc_list = [to_float(x) for x in my_numbers]
+print(lc_list)
+
+my_numlist = list(range(10))            # range() is an iterator
+print(my_numlist)
+
+my_second_range = range(10, 20)
+for x in my_second_range:
+    print(f'x+2={x + 2}')
+
+#####################################################################################
+# Generator expressions return iterators as map() does
+print("Generator expression")
+gen_list = list(to_float(x) for x in my_numbers)
+print(gen_list)
+
+# This shows that the generator expression returns an iterator. In this
+# case it is used in the loop examples below. The for loop is the easiest
+# to understand.
+gen_iterator = (to_float(x) for x in my_numbers)
+num = next(gen_iterator)
+while num is not None:
+    print(f'num={num}')
+    try:
+        num = next(gen_iterator)
+    except StopIteration:
+        break
+
+# Note! The iterator is exhausted by the while loop above, it has to be
+# re-initialized for the for-loop to work.
+gen_iterator = (to_float(x) for x in my_numbers)
+for num in gen_iterator:
+    print(f'num={num}')

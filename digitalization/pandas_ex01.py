05da5e76bdc34c814640ea285049d8f810fbf5ef
tryggves <tryggve.sorensen@gmail.com>
2021-10-29 23:32:30 +0200

    Pandas apply function to entire dataframe and specific columns.

--- digitalization/pandas_ex01.py
+++ digitalization/pandas_ex01.py
@@ -2,20 +2,11 @@
 # See: https://www.w3resource.com/python-exercises/pandas/datetime/pandas-datetime-exercise-9.php
 # See: https://pandas.pydata.org/docs/reference/api/pandas.to_datetime.html
 
-from datetime import datetime
-from cognite.client import CogniteClient
-import cognite.client.exceptions
-import os
-import time
 import json
-import numbers
-import pandas as pd
-import numpy as np
 from typing import List, Dict
 
-import pandas as pd
-import datetime as dt
 import numpy as np
+import pandas as pd
 
 # Map protobuf Iongeo.vessel_turn.ArcType to integers
 arc_type_map = {
@@ -24,6 +15,18 @@ arc_type_map = {
     "CLOCKWISE": 1
 }
 
+
+def apply_map_on_column():
+    arc_type_data = {"timestamp": [100, 101, 102, 103], "arc_type": ['ANTICLOCKWISE', 'STRAIGHT', 'STRAIGHT',
+                                                                     'CLOCKWISE']}
+    at_df = pd.DataFrame(arc_type_data)
+    print('=== arc_type dataframe =================================')
+    print(at_df)
+
+    # Convert column arc_type values to int map
+    at_df.arc_type = at_df.arc_type.apply(lambda x: arc_type_map[x])
+    print('=== after applying arc_type_map ========================')
+    print(at_df)
 
 
 def make_columns_map(vessel: str, source: str) -> Dict[str, str]:
@@ -78,3 +81,31 @@ print(ts_epoc.value // 10**6)
 # Convert string to datetime
 ts_epoc2 = pd.to_datetime("2020-04-29T00:01:00.000500Z")
 print(ts_epoc2.value // 10**6)
+
+# Create a data frame from a dictionary
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+
+print('=== df ==============================')
+print(df)
+
+# Apply a function
+new_df = df.apply(lambda x: x**2, axis=1)
+print('== After apply... ========================')
+print(new_df)
+
+new_df = df.apply(np.sum, axis=0)
+print('== After sum axis=0 ... ========================')
+print(new_df)
+
+new_df = df.apply(np.sum, axis=1)
+print('== After sum axis=1 ... ========================')
+print(new_df)
+
+# Apply function on one column only
+# NOtE: you can refer to a full column by subscripting dataframe with column name
+df.a = df.a.apply(lambda x: x+3)
+print('== After applying to column a on df ===================')
+print(df)
+
+apply_map_on_column()

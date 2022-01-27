5edd07782b216ce23445f9c8d5a9e3756ce07136
tryggve.sorensen <tryggve.sorensen@pgs.com>
2022-01-27 14:21:12 +0100

    Pandas dataframe cleaning example
    Efficient methods.

--- digitalization/pandas_cleaning.py
+++ digitalization/pandas_cleaning.py
@@ -39,6 +39,39 @@ def standard_scaler_apply(pandas_series: pd.Series,  mean_pandas_series: float,
     return scaled_pandas_series
 
 
+def standard_scaler_map(pandas_element: int, mean_pandas_series: float,
+                             std_pandas_series: float) -> float:
+    """
+    Use pd:Series.map() to map through the elements in Pandas Series
+    :param pandas_element:
+    :param mean_pandas_series:
+    :param std_pandas_series:
+    :return:
+    """
+    scaled_pandas_element = (pandas_element - mean_pandas_series) / std_pandas_series
+    return scaled_pandas_element
+
+
+def standard_scaler_vectorized_series(pandas_series: pd.Series) -> pd.Series:
+    """
+    Vectorized operation across Pandas Series
+    :param pandas_series:
+    :return:
+    """
+    scaled_series = (pandas_series - np.mean(pandas_series)) / np.std(pandas_series)
+    return scaled_series
+
+
+def standard_scaler_vectorized_array(numpy_array: np.array) -> np.array:
+    """
+    Vectorized operation across Numpy arrays
+    :param numpy_array:
+    :return:
+    """
+    scaled_array = (numpy_array - np.mean(numpy_array)) / np.std(numpy_array)
+    return scaled_array
+
+
 # %%
 # Create 3 data series each containing 100 mill elements
 np.random.seed(42)
@@ -53,23 +86,56 @@ data_df = pd.DataFrame({'random_numbers_1': random_numbers_1,
 
 print(data_df.info)
 
-# %%time
-
+# ===================================================================
 # Calculate mean and standard deviation of column random_numbers_2
 mean_pandas_series = np.mean(data_df['random_numbers_2'])
 std_pandas_series = np.std(data_df['random_numbers_2'])
 
 print(f'Mean: {mean_pandas_series}, Std: {std_pandas_series}')
 
+# ===================================================================
 # This call will take extremely long to execute
 # data_df = standard_scaler_iterrows(pandas_dataframe=data_df, mean_pandas_series=mean_pandas_series,
 #                                   std_pandas_series=std_pandas_series)
 # print(data_df.info)
 
+# ===================================================================
 # This call will calculate on all elements of a column using apply() function
-t_start = time.time()
-data_df['scaled_random_numbers_2'] = data_df['random_numbers_2'].apply(standard_scaler_apply,
-                                                                       args=(mean_pandas_series, std_pandas_series))
-t_end = time.time()
-print(data_df.info)
-print(f'Duration: {t_end-t_start}')
+# print(f'Use standard_scaler_apply()...')
+# t_start = time.perf_counter()
+# data_df['scaled_random_numbers_2'] = data_df['random_numbers_2'].apply(standard_scaler_apply,
+#                                                                        args=(mean_pandas_series, std_pandas_series))
+# t_end = time.perf_counter()
+# print(data_df.info)
+# print(f'Duration: {t_end-t_start:0.4f} seconds')
+
+# ===================================================================
+# This call will calculate on all elements of column using map() function
+# print(f'Use standard_scaler_map()...')
+# t_start = time.perf_counter()
+# data_df['scaled_random_numbers_2'] = data_df['random_numbers_2'].map(lambda x: standard_scaler_map(x, mean_pandas_series=mean_pandas_series, std_pandas_series=std_pandas_series))
+# t_end = time.perf_counter()
+# print(data_df.info)
+# print(f'Duration: {t_end-t_start:0.4f} seconds')
+
+# ===================================================================
+# This call will calculate on all elements of column using vectorized operation over Pandas Series
+print(f'Use standard_scaler_vectorized_series()...')
+t_start = time.perf_counter_ns()
+data_df['scaled_random_numbers_2'] = standard_scaler_vectorized_series(data_df['random_numbers_2'])
+t_end = time.perf_counter_ns()
+# print(data_df.info)
+duration = (t_end-t_start)/10e6
+print(f'Duration: {duration:0.4f} milliseconds')
+
+
+# ===================================================================
+# This call will calculate on all elements of column using vectorized operation over Numpy array
+print(f'Use standard_scaler_vectorized_array()...')
+t_start = time.perf_counter_ns()
+data_df['scaled_random_numbers_2'] = standard_scaler_vectorized_array(data_df['random_numbers_2'].values)
+t_end = time.perf_counter_ns()
+# print(data_df.info)
+duration = (t_end-t_start)/10e6
+print(f'Duration: {duration:0.4f} milliseconds')
+

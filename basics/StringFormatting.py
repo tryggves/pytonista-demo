093002472bc77fd8b9f8d3bfaf74a9a1fef353bd
tryggve.sorensen <tryggve.sorensen@pgs.com>
2022-08-16 15:05:49 +0200

    String formatting examples with f-strings

--- basics/StringFormatting.py
+++ basics/StringFormatting.py
@@ -4,6 +4,9 @@
 # This example needs more work.
 # See: https://www.w3schools.com/python/python_strings.asp
 
+# String formatting
+# https://realpython.com/python-formatted-output/#the-string-format-method-arguments
+
 # What are the most useful features
 # Strings contatenation
 # Strings filtering including reqular expressions
@@ -21,12 +24,38 @@ year = 2018
 month = 'September'
 month_oct = 'October'
 year_2020 = 2020
-print(f'Hello format string of {month} {year}')
-# Here is another way to do the above:
-formatText = "Hello format string of {} {}"
-print(formatText.format(month_oct, year_2020))
 
-# Print multi line text constant.
+print("\nF-string formatting introduced in version 3.6")
+print(f'Hello format string of {month} {year}')
+print(f'Hello format string of {month_oct} {year_2020}')
+
+# Replacement field format:
+# {[<name>][!<conversion>][:<format_spec>]}
+# conversion is not much used in practice
+
+print("\nUsing str.format")
+# Field format with <name> using str.format and F-string
+formatText1 = "Hello format string of {} {}"
+print(formatText1.format(month_oct, year_2020))
+print(f'Hello format string of {month_oct} {year_2020}')
+formatText2 = "Hello format string of {0} {1}"
+print(formatText2.format(month, year))
+print(f'Hello format string of {month} {year}')
+formatText3 = "Hello format string of {month} {year}"
+print(formatText3.format(month=month_oct, year=year_2020))
+print(f'Hello format string of {month_oct} {year_2020}')
+
+# Access dictionary elements from replacement fields.
+my_dict = {
+    'first_name': 'Tryggve',
+    'age': 59
+}
+formatText4 = "{d[first_name]}'s age is {d[age]}"
+print(formatText4.format(d=my_dict))
+print(f'{my_dict["first_name"]}\'s age is {my_dict["age"]}')
+print(f"{my_dict['first_name']}'s age is {my_dict['age']}")
+
+print("\nPrint multi line text constant.")
 print(multiline_text)
 
 # Print string in uppercase

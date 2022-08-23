582570effafaa62c40f088b41498a88c52e26f99
tryggve.sorensen <tryggve.sorensen@pgs.com>
2022-08-23 09:11:27 +0200

    Testing custom metric with labels

--- prometheus/prom_ex01.py
+++ prometheus/prom_ex01.py
@@ -23,11 +23,22 @@ if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
+    # num_datapoints_metric is immutable, use the object intstance returned when setting label values.
     num_datapoints_metric = Gauge('orca_cdf_datapoints_inserted', 'ORCA netsolution vessel datapoints ingested',
                            labelnames=['vessel', 'cdf_datapoint_type'])
+
+    vessel_status_metric = Gauge('vessel_status', 'Vessel status registered by Vesselbus',
+                                  labelnames=['vessel'])
+
     while True:
         # process_request(random.random())
+        # Set metric 1
         orca_metric = num_datapoints_metric.labels(vessel='VAN', cdf_datapoint_type='orca_netsolution_vessel')
         orca_metric.set(random.randint(1000, 5000))
         # Sleep 30 seconds before setting next datapoint
+
+        # Set metric 2
+        vessel_status = vessel_status_metric.labels(vessel='VAN')
+        vessel_status.set(1)
+
         time.sleep(30)

e293cf85808432f30a6d3488372f07c52b8f05f2
tryggves <tryggve.sorensen@gmail.com>
2022-10-08 06:22:34 +0200

    Decorators

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -56,7 +56,24 @@ def timer(func):
         value = func(*args, **kwargs)
         end_time = time.perf_counter()  # 2
         run_time = end_time - start_time  # 3
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        print(f"Finished {func.__name__!r} in {run_time:.6f} secs")
         return value
 
     return wrapper_timer
+
+
+# Here is a nice debug feature
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        # repr is a function for creating human readable string of passed paraemter
+        args_repr = [repr(a) for a in args]                      # 1
+        # v!r means pass v to repr()
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug
--- decorators/ex_04_decorator.py
+++ decorators/ex_04_decorator.py
@@ -1,15 +1,17 @@
 #
 # include module with wrapper
 #
+import math
 import time
 
-from decorators import do_twice, repeat, timer
+# from decorators import do_twice, repeat, timer
+import decorators
 
 
 def main():
     print('==== Example 1 =====================================')
 
-    @do_twice
+    @decorators.do_twice
     def say_hello():
         print('Hello world')
 
@@ -18,7 +20,7 @@ def main():
     print('==== Example 2 =====================================')
     # Wrapper supports any number of arguments
 
-    @do_twice
+    @decorators.do_twice
     def greet_people(**kwargs):
         # zip makes iterable of tuples out of elements of iterables passed to it
         # in this case the typles are key, value pairs of kwargs
@@ -29,7 +31,9 @@ def main():
 
     greet_people(per='per', anders='anders')
 
-    @do_twice
+    print('==== Example 3 =====================================')
+
+    @decorators.do_twice
     def greet_people_2(**kwargs):
         # Here is another way to iterate kwargs, why the zip function above?
         for key, value in kwargs.items():
@@ -37,15 +41,47 @@ def main():
 
     greet_people_2(person1='kåre', person2='magnus')
 
-    @repeat(num_times=2)
+    print('==== Example 4 =====================================')
+
+    @decorators.repeat(num_times=2)
     def hello(name=None):
         return f'Hello {name}'
     print(hello('world'))
 
-    @timer
+    print('''==== Example 5 =====================================
+    Nested decorators''')
+
+    # Nested decorators
+    @decorators.debug
+    @decorators.timer
     def do_work():
-        time.sleep(5)
+        time.sleep(2)
     do_work()
+
+    print('''==== Example 6 =====================================
+    Using debug wrapper''')
+
+    @decorators.debug
+    def make_greeting(name, age=None):
+        """Create greeting string"""
+        if age is None:
+            return f"Happy birthday, {name}!"
+        else:
+            return f"Happy {age} birthday, {name}!"
+
+    print(make_greeting('Per'))
+    print(make_greeting('Kåre', 50))
+
+    print('''==== Example 7 =====================================
+        Apply a decorator to a standard library function''')
+    # Apply a decorator to a standard library function
+    math.factorial = decorators.debug(math.factorial)
+
+    def approximate_e(terms=18):
+        return sum(1 / math.factorial(n) for n in range(terms))
+
+    e = approximate_e(7)
+    print(f'Approximate e={e}')
 
 
 if __name__ == '__main__':

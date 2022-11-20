bd68ac8868db7aedbe379af50667f0b94d433055
tryggves <tryggve.sorensen@gmail.com>
2022-11-20 10:21:08 +0100

    Getter, setter and make state private to object instance.

--- objects/Class_methods1.py
+++ objects/Class_methods1.py
@@ -48,6 +48,49 @@ class MyGraphClass:
         del self._x
 
 
+# Implement MyGraphClass with the state (edges) as member attributes.
+# Hens change on the state will only impact one object instance and not
+# all instances.
+class MyGraphClassStatePrivate:
+    def __init__(self):
+        self._x = 10
+        self._edges = [5, 6, 7, 8, 9]
+        self._edge_count = len(self._edges)
+        self._count_updates = 0
+
+    @property
+    def edges(self) -> List[int]:
+        return self._edges
+
+    @edges.setter
+    def edges(self, new_value: List[int]) -> None:
+        self._edges = new_value
+        self._edge_count = len(self._edges)
+        self._count_updates += 1
+
+    @property
+    def edge_count(self) -> int:
+        return self._edge_count
+
+    @property
+    def count_updates(self) -> int:
+        return self._count_updates
+
+    # Member attribute getter/setter/deleter
+    @property
+    def x(self):
+        """I am the 'x' property."""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        del self._x
+
+
 def main():
     print('===================================================================================')
     print(" Testing static and class methods.")
@@ -78,6 +121,31 @@ def main():
     MyGraphClass.class_static_method()
     # Can call static method through object instance
     my_second_graph.class_static_method()
+
+    print()
+    print('===================================================================================')
+    print('Test class where state is protected')
+    print('===================================================================================')
+    print('Create first graph')
+    my_first_graph = MyGraphClassStatePrivate()
+    print(f'Before modifying graph edge_count:  {my_first_graph.edge_count}')
+
+    # Make a new graph
+    my_new_edges = [1, 453, 3498, 3, 3, 38, 348, 3498, 10, 11, 342]
+
+    # This calls the object setter function
+    my_first_graph.edges = my_new_edges
+    print(f'After modifying graph edge_count:  {my_first_graph.edge_count}')
+
+    # Instantiate a new graph
+    print(f'Create second graph')
+    my_second_graph = MyGraphClassStatePrivate()
+    print(f'After creating second graph edge_count:  {my_second_graph.edge_count}')
+
+    my_second_graph.edges = [1, 2, 3, 4, 5, 6]
+    print('\nAfter new modification to graph')
+    print(f'my_first_graph edge_count: {my_first_graph.edge_count}\tcount_updates: {my_first_graph.count_updates}')
+    print(f'my_second_graph edge_count: {my_second_graph.edge_count}\tcount_updates: {my_first_graph.count_updates}')
 
 
 if __name__ == '__main__':

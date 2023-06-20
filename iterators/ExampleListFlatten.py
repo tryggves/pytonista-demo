ca679b20ecee7e2b8b2a096c9bcf15b9d6700804
tryggves <tryggve.sorensen@gmail.com>
2023-06-20 22:22:49 +0200

    Flatten list exmaple

--- iterators/ExampleListFlatten.py
+++ iterators/ExampleListFlatten.py
@@ -8,6 +8,12 @@ def flatten_extend(matrix):
         flat_list.extend(row)
     return flat_list
 
+def flatten_concatenation(matrix):
+    flat_list = []
+    for row in matrix:
+        flat_list += row
+    return flat_list
+
 my_matrix = [
     [1, 2, 3],
     [4, 5, 6],
@@ -15,4 +21,7 @@ my_matrix = [
 ]
 
 fl = flatten_extend(my_matrix)
-print(fl)
+print(f'fl: {fl}')
+
+flc = flatten_concatenation(my_matrix)
+print(f'flc: {flc}')

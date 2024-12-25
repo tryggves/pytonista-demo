f60dda5b482875ed141d3fd6f94362811a09ff12
Tryggve Sørensen <tryggve.sorensen@me.com>
2024-12-25 11:47:17 +0100

    More linear algebra

--- digitalization/digitalisation.py
+++ digitalization/digitalisation.py
@@ -11,10 +11,26 @@ print("data type:", s.dtype)
 print("s:\n", s)
 
 # Create an identity matrix with dimension 3x3
-t = np.eye(3, 3)
-print(f"t:\n {t}")
+I = np.eye(3, 3)
+print(f"I=\n {I}")
 
 u = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]])
-print(f"u:\n {u}")
+print(f"u=\n {u}")
 v = s + u
 print(f"s+u=\n {v}")
+
+# Try some linear algebra
+print("\nAxis rotation example using matrix multiplication")
+# Create a 3x3 matrix
+A = np.array([[1, 0, 0], 
+              [0, 1, 0], 
+              [0, 0, 1]])
+print(f"A=\n {A}")
+
+# Create a 3x1 matrix as input data to the rotation
+x = np.array([[1], [2], [3]])
+print(f"x=\n {x}")
+
+# Multiply the two matrices
+b = A @ x
+print(f"b=A @ x\n {b}")

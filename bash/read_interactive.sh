1be9a0811032eb945c3bdf781934d5049367c644
Tryggve Sørensen <tryggve.sorensen@pgs.com>
2025-03-06 20:03:08 +0100

    Comment code.

--- bash/read_interactive.sh
+++ bash/read_interactive.sh
@@ -2,6 +2,7 @@ my_input=""
 echo "Before input: $my_input"
 echo ""
 
+# Flag -p prompts user for input and stores the response in $my_input variable
 read -p "What is your input: " my_input
 echo ""
 echo "After input: $my_input"
